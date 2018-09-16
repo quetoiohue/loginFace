6d9695faa192d7dc43e2e1305cd3b97f37a3e97d
Quang <quetoiohue@gmail.com>
2018-09-16 17:27:26 +0700

    loginFace

---
+++ app/src/main/java/com/example/dell/loginface/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dell.loginface;
+
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView sign_up_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
